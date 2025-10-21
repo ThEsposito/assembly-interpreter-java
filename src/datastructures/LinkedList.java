8a0d005b8eccbdbb92f2bd6f6d064c0170a68cd9
ThEsposito <thesposito.estudos@gmail.com>
2025-10-21 19:26:01 -0300

    LinkedList insert operations

--- src/datastructures/LinkedList.java
+++ src/datastructures/LinkedList.java
@@ -1,5 +1,7 @@
 package datastructures;
 
+import exceptions.EmptyListException;
+
 public class LinkedList<T> {
     private Node<T> head;
     private int size;
@@ -18,19 +20,66 @@ public class LinkedList<T> {
     }
 
     public Node<T> getFirst() {
+        if(isEmpty()) throw new EmptyListException();
         return this.head;
     }
 
     public Node<T> getLast() {
+        if(isEmpty()) throw new EmptyListException();
+
         Node<T> aux = head;
-        
+
         while(aux.getNext() != null) {
             aux = aux.getNext();
         }
+        return aux;
+    }
+    // TODO: test
+    public Node<T> get(int idx){
+        if(isEmpty()) throw new EmptyListException();
+        if(idx < 0 || idx >= size) throw new IndexOutOfBoundsException();
 
+        Node<T> aux = head;
+        for(int i=0; i<idx; i++){
+            aux = aux.getNext();
+        }
         return aux;
     }
 
+    public void addFirst(T e){
+        head = new Node<>(e, head);
+        size++;
+    }
 
+    public void addLast(T e){
+        if(isEmpty()) this.head = new Node<>(e);
+        else {
+            Node<T> aux = head;
 
+            while(aux.getNext() != null){
+                aux = aux.getNext();
+            }
+
+            aux.setNext(new Node<>(e));
+        }
+        size++;
+    }
+
+    // TODO: test
+    public void insert(T e, int idx){
+        if(idx < 0 || idx > size) throw new IndexOutOfBoundsException();
+
+        if(idx == size) addLast(e);
+        else if(idx == 0) addFirst(e);
+        else {
+            Node<T> aux = head;
+            for(int i=0; i<idx-1; i++){
+                aux = aux.getNext();
+            }
+            Node<T> newElement = new Node<>(e, aux.getNext());
+            aux.setNext(newElement);
+
+            size++;
+        }
+    }
 }
