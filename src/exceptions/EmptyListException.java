025b23d51a01411825a1389fb186155fa6cf21d5
Kauê Lima <kauelima557@gmail.com>
2025-11-04 13:50:11 +0000

    OrderedLL

--- src/exceptions/EmptyListException.java
+++
@@ -1,9 +0,0 @@
-package exceptions;
-
-public class EmptyListException extends RuntimeException {
-    public EmptyListException(String message) {
-        super(message);
-    }
-
-    public EmptyListException() {}
-}
