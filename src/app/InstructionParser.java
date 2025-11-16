057e0a7c55b437761bac6fc6d4eba0d54a63600a
Kauê Lima <kauelima557@gmail.com>
2025-11-16 19:24:49 +0000

    printAll + clear + contains methods

--- src/app/InstructionParser.java
+++ src/app/InstructionParser.java
@@ -4,5 +4,6 @@ public class InstructionParser {
     public static Instruction parse(String rawLine){
         // Lógica pra converter cada instrução num objeto
         // Se atentar no número de argumentos que cada instrução requer
+        return null;
     }
 }
