208aadc0db1391784d30b04ccba0e3ee4e509fc8
ThEsposito <thesposito.estudos@gmail.com>
2025-11-17 18:48:03 -0300

    Interpreter sugestions

--- src/app/Interpreter.java
+++ src/app/Interpreter.java
@@ -1,44 +1,63 @@
 package app;
 
+import datastructures.Node;
 import datastructures.OrderedLL;
 
 public class Interpreter{
-    public void execute(OrderedLL<Instruction> instructions){
-        Registers regs = new Registers(); // Inicializa os registradores
+    Registers regs;
 
-        Node<Instruction> current = instructions.getHead();
+    public Interpreter() {
+        this.regs = new Registers();
+    }
+
+    public void execute(OrderedLL<Instruction> instructions){
+        Node<Instruction> current = instructions.getHead(); // É uma má prática expor os Nodes assim??
 
         while(current != null){
             Instruction instr = current.getData();
             Node<Instruction> next = current.getNext(); // Nó padrão para a próxima iteração
             boolean jumped = false; // Flag para controlar se um salto ocorreu
+            /*
+            Edge cases:
+             - Excesso/falta de argumentos
+             - Registrador indefinido (não atribuído)
+             - Valor imediato (inteiro) no argumento X (é proibido)
+             - Registradores com mais de um caractere (!Util.isNumber(arg2) && arg2.length() > 1);
+
+             Ideias:
+              - Ao invés de iterar sobre os nodes, utilizar um for (e reatriuir o 'i' no jnz se necessário)
+              - Passar a instrução para cada método, ao invés dos args (permitiria melhor validação) (um chute)
+              - Um método só pra validar??? Não sei como fica mais bonito
+              -
+             */
+
             try{
                 switch(instr.getOpcode()){
-                    case MOVE:
+                    case "MOVE":
                         executeMov(regs, instr.getArg1(), instr.getArg2());
                         break;
-                    case INC:
+                    case "INC":
                         executeInc(regs, instr.getArg1());
                         break;
-                    case DEC:
+                    case "DEC":
                         executeDec(regs, instr.getArg1());
                         break;            
-                    case ADD:
+                    case "ADD":
                         executeAdd(regs, instr.getArg1(), instr.getArg2());
                         break;
-                    case SUB:
+                    case "SUB":
                         executeSub(regs, instr.getArg1(), instr.getArg2());
                         break;
-                    case MUL:
+                    case "MUL":
                         executeMul(regs, instr.getArg1(), instr.getArg2());
                         break;
-                    case DIV:
+                    case "DIV":
                         executeDiv(regs, instr.getArg1(), instr.getArg2());
                         break;
-                    case OUT:
+                    case "OUT":
                         executeOut(regs, instr.getArg1());
                         break;
-                    case JNZ:
+                    case "JNZ":
                     // Este método deve:
                     // 1. Verificar o valor de arg1 em 'regs'.
                     // 2. Se NÃO for zero, ele deve encontrar o Nó da linha (arg2) na lista.
@@ -63,5 +82,7 @@ public class Interpreter{
             }
             current = current.getNext();
         }
+
+        regs.clear(); // Reseta os registradores. Lembrar de fazer isso ao lançar qquer exceção
     }
 }
